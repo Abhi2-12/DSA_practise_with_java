333c25a20bd9454f678ddc7c1e2b2224e013605d
Abhi2-12 <anamulhassan212@gmail.com>
2025-10-21 14:50:21 +0600

    binary search using recursive

---
+++ Recursion/binarySarch.java
@@ -0,0 +1,38 @@
+package Recursion;
+
+public class binarySarch {
+
+    static int binarySarchRecursive(int arr[],int low,int high,int x){
+        if(high >= low) {
+            int mid = low + (high - low) / 2;
+
+            //if element in the middle 
+            if(arr[mid] == x)
+            return mid;
+
+            if(arr[mid] > x)
+            return binarySarchRecursive(arr, low, mid - 1, x);
+
+            return binarySarchRecursive(arr, mid + 1, high, x);
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int x = 7;
+        int n = arr.length;
+
+        int result = binarySarchRecursive(arr, 0, n-1, x);
+        if (result == -1)
+            System.out.println("element not present ");
+
+        else 
+            System.out.println("element is present at index "+ result);
+
+
+    }
+
+    
+}
